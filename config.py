9327aaec85dae8f1f6eea35eaa7e127c72728ac1
mssaued49-stack <mssaued49@gmail.com>
2025-11-06 16:44:13 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+import os
+
+# إعدادات البوت
+BOT_TOKEN = '8122538449:AAGE9NIO18L6OqF5DZlQxsIK6x7LdHDJwmA'
+REQUIRED_CHANNEL = '@Msayedinfoo'
+DB_NAME = 'bot_data.db'
+
+# إعدادات التوقيت
+WARNING_DELETE_TIMEOUT = 180  # 3 دقائق بالثواني
+
+# إعدادات التطوير
+DEBUG = True
