30cb195cb8042e606b3593dfba1982e439fa5b6e
mssaued49-stack <mssaued49@gmail.com>
2025-11-06 16:44:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 # config.py
 import os
 
-# إعدادات البوت
-BOT_TOKEN = '8122538449:AAGE9NIO18L6OqF5DZlQxsIK6x7LdHDJwmA'
-REQUIRED_CHANNEL = '@Msayedinfoo'
-DB_NAME = 'bot_data.db'
+# إعدادات البوت - يتم أخذ التوكن من متغيرات البيئة
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8122538449:AAGE9NIO18L6OqF5DZlQxsIK6x7LdHDJwmA')
+REQUIRED_CHANNEL = os.environ.get('REQUIRED_CHANNEL', '@Msayedinfoo')
+DB_NAME = os.environ.get('DB_NAME', 'bot_data.db')
 
 # إعدادات التوقيت
 WARNING_DELETE_TIMEOUT = 180  # 3 دقائق بالثواني
 
 # إعدادات التطوير
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+# إعدادات Railway
+PORT = int(os.environ.get('PORT', 8080))
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL', '')
